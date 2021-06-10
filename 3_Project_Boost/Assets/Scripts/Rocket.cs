a9d3730ffab151d2c7d9cffd40a3a36af7e9e122
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2021-06-10 11:33:57 +0530

    3_Project_Boost

---
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	 Rigidbody rigidBody;
+	AudioSource audiosource;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+		audiosource= GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		ProcessInput();
+	}
+
+	 void ProcessInput()
+	 {
+		 if (Input.GetKey(KeyCode.Space))
+		 {
+			 rigidBody.AddRelativeForce(Vector3.up);
+			 if (!audiosource.isPlaying)
+			 {
+				 audiosource.Play();
+			 }
+			 else
+			 {
+				 audiosource.Stop();
+			 }
+		 }
+
+		 if (Input.GetKey(KeyCode.A))
+		 {
+			 transform.Rotate(Vector3.forward);
+		 }
+		 else if (Input.GetKey(KeyCode.D))
+		 {
+			 transform.Rotate(-Vector3.forward);
+		 }
+	 }
+}
